af68fb4d2f2ebec41137e2b2101eb9620baa04f7
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-05 13:49:52 +0300

    Update fire_analysis.py
    add functions

--- apps/fire_analysis.py
+++ apps/fire_analysis.py
@@ -1,16 +1,23 @@
 """
 The page for fire analysis page.
 """
+
+# Standard libraries
 from io import StringIO
 import datetime
 from datetime import date
+import tempfile
+import os
+import uuid
 
+# Third party libraries
 import streamlit as st
 import geemap.foliumap as geemap
 import ee
 import folium
+import geopandas as gpd
 
-
+# Local libraries
 from . import rois  # Why i am getting pylint error? code works fine.
 
 IMAGE_COLLECTION = "COPERNICUS/S2"
@@ -18,6 +25,41 @@ MAP_WIDTH = 950
 MAP_HEIGHT = 600
 CRS = "epsg:4326"  # Coordinate Reference System
 
+
+def map_search(folium_map):  # sourcery skip: use-named-expression
+    """
+    The function to generate the search box above the map.
+    """
+    keyword = st.text_input("Bölge arayın:", "")
+    if keyword:
+        locations = geemap.geocode(keyword)
+        if locations is not None and len(locations) > 0:
+            str_locations = [str(g)[1:-1] for g in locations]
+            location = st.selectbox("Bölge seçin:", str_locations)
+            loc_index = str_locations.index(location)
+            selected_loc = locations[loc_index]
+            lat, lng = selected_loc.lat, selected_loc.lng
+            folium.Marker(location=[lat, lng], popup=location).add_to(folium_map)
+            folium_map.set_center(lng, lat, 12)
+            st.session_state["zoom_level"] = 12
+
+@st.cache
+def uploaded_file_to_gdf(data):
+    """
+    The function to convert uploaded file to geodataframe.
+    """
+    _, file_extension = os.path.splitext(data.name)
+    file_id = str(uuid.uuid4())
+    file_path = os.path.join(tempfile.gettempdir(), f"{file_id}{file_extension}")
+
+    with open(file_path, "wb") as file:
+        file.write(data.getbuffer())
+
+    if not file_path.lower().endswith(".kml"):
+        return gpd.read_file(file_path)
+
+    gpd.io.file.fiona.drvsupport.supported_drivers["KML"] = "rw"
+    return gpd.read_file(file_path, driver="KML")
 
 def app():
     # sourcery skip: remove-redundant-if, remove-redundant-pass, use-named-expression
