c3d4668c8734ec94c6390b9f96e009d3897cd3ae
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-06 13:11:05 +0300

    it works noww

--- apps/fire_analysis.py
+++ apps/fire_analysis.py
@@ -25,6 +25,8 @@ MAP_WIDTH = 950
 MAP_HEIGHT = 600
 CRS = "epsg:4326"  # Coordinate Reference System
 
+def string_to_date(date_string):
+    return 
 
 def map_search(folium_map):  # sourcery skip: use-named-expression
     """
@@ -84,7 +86,7 @@ sayfasını ziyaret edebilirsiniz.
         """
     )
 
-    row1_col1, row1_col2 = st.columns([2, 1])
+    col1, col2 = st.columns([2, 1])
 
     if st.session_state.get("zoom_level") is None:
         st.session_state["zoom_level"] = 4
@@ -97,29 +99,7 @@ sayfasını ziyaret edebilirsiniz.
         plugin_LatLngPopup=False,
     )
 
-    with row1_col1:
-        st.info(
-            "Adımlar: Harita üzerinde poligon çizin -> GeoJSON olarak export edin"
-            "-> Uygulumaya upload edin"
-            "-> Submit tuşuna tıklayın."
-        )
-
-        keyword = st.text_input("Bölge arayın:", "")
-        if keyword:
-            locations = geemap.geocode(keyword)
-            if locations is not None and len(locations) > 0:
-                str_locations = [str(g)[1:-1] for g in locations]
-                location = st.selectbox("Bölge seçin:", str_locations)
-                loc_index = str_locations.index(location)
-                selected_loc = locations[loc_index]
-                lat, lng = selected_loc.lat, selected_loc.lng
-                folium.Marker(location=[lat, lng], popup=location).add_to(main_map)
-                main_map.set_center(lng, lat, 12)
-                st.session_state["zoom_level"] = 12
-
-        main_map.to_streamlit(height=400)
-
-    with row1_col2:
+    with col2:
         data = st.file_uploader(
             "ROI olarak kullanmak için GeoJSON dosyası ekleyin 😇👇",
             type=["geojson", "kml", "zip"],
@@ -130,26 +110,39 @@ sayfasını ziyaret edebilirsiniz.
             ["Yüklenilen GeoJSON"] + list(rois.fire_cases.keys()),
             index=0,
         )
+        pre_fire = date.today() - datetime.timedelta(days=1)
+        post_fire = date.today()
 
-        geometry = None
-        if selected_roi == "Yüklenilen GeoJSON":  # rois coming from the user
-            pre_fire_date = st.date_input(
-                "Yangın başlangıç tarihi", date.today() - datetime.timedelta(days=1)
-            )
+        if selected_roi != "Yüklenilen GeoJSON":  # rois coming from fire_cases
+            st.session_state["roi"] = rois.fire_cases[selected_roi]["region"]
+            pre_fire = date.fromisoformat(rois.fire_cases[selected_roi]["date_range"][0])
+            post_fire = date.fromisoformat(rois.fire_cases[selected_roi]["date_range"][1])
 
-            post_fire_date = st.date_input("Yangın bitiş tarihi", date.today())
-            if data:
-                geojson = StringIO(data.getvalue().decode("utf-8"))
-                geometry = ee.Geometry(geojson)
+        elif data:  # rois coming from users
+            gdf = uploaded_file_to_gdf(data)
+            st.session_state["roi"] = geemap.gdf_to_ee(gdf)
 
-        else:  # rois coming from fire_cases
-            geometry = rois.fire_cases[selected_roi]["region"]
-            pre_fire_date = rois.fire_cases[selected_roi]["date_range"][0]
-            post_fire_date = rois.fire_cases[selected_roi]["date_range"][1]
+        pre_fire_date = st.date_input("Yangın başlangıç tarihi", pre_fire)
+        post_fire_date = st.date_input("Yangın bitiş tarihi", post_fire)
+
+    with col1:
+        st.info(
+            "Adımlar: Harita üzerinde poligon çizin -> GeoJSON olarak export edin"
+            "-> Uygulumaya upload edin"
+            "-> Submit tuşuna tıklayın."
+        )
+        map_search(main_map)
+
+        
+        if st.session_state.get("roi"):
+            main_map.add_layer(st.session_state["roi"])
+            main_map.center_object(st.session_state["roi"])
+
+        main_map.to_streamlit(height=400)
 
     # now we have geometry and dates
     # also we need the geometry as ee.Geometry
-
-    print(geometry, pre_fire_date, post_fire_date)
+    print(pre_fire_date, post_fire_date)
+    print(st.session_state.get("roi"))
     image_collection = ee.ImageCollection(IMAGE_COLLECTION)
     print(image_collection)
