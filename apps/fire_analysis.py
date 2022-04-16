f733614f4152b4c974e15f42f066dd7afe763f8a
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-04-16 18:30:43 +0300

    Update fire_analysis.py
    Added dnbr

--- apps/fire_analysis.py
+++ apps/fire_analysis.py
@@ -22,6 +22,7 @@ from . import rois
 SENTINEL = "COPERNICUS/S2"
 MAP_HEIGHT = 600
 CRS = "epsg:4326"  # Coordinate Reference System
+DAY_WINDOW = 6
 
 rgb_vis_params = {
     "bands": ["B4", "B3", "B2"],
@@ -122,10 +123,10 @@ def app():
         pre_fire_date = st.date_input("Yangın başlangıç tarihi", pre_fire)
         post_fire_date = st.date_input("Yangın bitiş tarihi", post_fire)
         dates = {
-            "prefire_start": str(pre_fire_date - datetime.timedelta(days=12)),
+            "prefire_start": str(pre_fire_date - datetime.timedelta(days=DAY_WINDOW)),
             "prefire_end": str(pre_fire_date),
             "postfire_start": str(post_fire_date),
-            "postfire_end": str(post_fire_date + datetime.timedelta(days=12)),
+            "postfire_end": str(post_fire_date + datetime.timedelta(days=DAY_WINDOW)),
         }
 
     with col1:
@@ -144,38 +145,32 @@ def app():
 
             imagery = ee.ImageCollection(SENTINEL)
 
-            roi_region = imagery.filterBounds(st.session_state["roi"])
-
-            prefire_roi_region = roi_region.filterDate(
+            prefire = imagery.filterDate(
                 dates["prefire_start"], dates["prefire_end"]
-            ).mosaic()
-            postfire_roi_region = roi_region.filterDate(
+            ).filterBounds(st.session_state["roi"])
+
+            postfire = imagery.filterDate(
                 dates["postfire_start"], dates["postfire_end"]
-            ).mosaic()
+            ).filterBounds(st.session_state["roi"])
 
-            main_map.add_layer(
-                prefire_roi_region.clip(st.session_state.get("roi")),
-                name="Prefire",
-                vis_params=rgb_vis_params,
-            )
-            main_map.add_layer(
-                postfire_roi_region.clip(st.session_state.get("roi")),
-                name="Postfire",
-                vis_params=rgb_vis_params,
-            )
+            pre_mos = prefire.median().clip(st.session_state["roi"])
+            post_mos = postfire.median().clip(st.session_state["roi"])
 
-            # False color layers
+            pre_nbr = pre_mos.normalizedDifference(["B8", "B12"])
+            post_nbr = post_mos.normalizedDifference(["B8", "B12"])
 
-            main_map.add_layer(
-                prefire_roi_region.clip(st.session_state.get("roi")),
-                name="Prefire false color",
-                vis_params=false_color_vis_params,
-            )
-            main_map.add_layer(
-                postfire_roi_region.clip(st.session_state.get("roi")),
-                name="Postfire false color",
-                vis_params=false_color_vis_params,
-            )
+            delta_nbr = pre_nbr.subtract(post_nbr).multiply(1000) # why are we mutliplying by 1000?
+
+            with open("assets/sld_intervals.xml", "r", encoding="utf-8") as file:
+                sld_intervals = file.read()
+
+            main_map.add_layer(pre_mos, rgb_vis_params, "Yangın öncesi görüntüler")
+            main_map.add_layer(post_mos, rgb_vis_params, "Yangın sonrası görüntüler")
+
+            main_map.add_layer(pre_mos, false_color_vis_params, "Yangın öncesi false color")
+            main_map.add_layer(post_mos, false_color_vis_params, "Yangın sonrası false color")
+
+            main_map.add_layer(delta_nbr.sldStyle(sld_intervals), name="dNBR")
 
             folium.map.LayerControl("topright", collapsed=False).add_to(main_map)
 
