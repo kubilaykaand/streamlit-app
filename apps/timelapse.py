62c2affc7ca3c85d9337d67692378f6c61d64f3d
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2022-04-26 13:22:44 +0300

    Update timelapse.py

--- apps/timelapse.py
+++ apps/timelapse.py
@@ -23,7 +23,39 @@ CRS = "epsg:4326"  # Coordinate Reference System
 DAY_WINDOW = 6
 INITIAL_DATE_WINDOW = 6
 
+
 def app():
 
     st.title("Timelapse")
     st.markdown("Belirlenmiş iki tarih arasında gif üreten sistem.")
+
+    col1, col2 = st.columns([2, 1])
+
+    if st.session_state.get("zoom_level") is None:
+        st.session_state["zoom_level"] = 4
+
+    main_map = geemap.Map(
+        basemap="ROADMAP",
+        plugin_Draw=True,
+        Draw_export=True,
+        locate_control=True,
+        plugin_LatLngPopup=False,
+    )
+
+    with col2:
+        data = st.file_uploader(
+            "ROI olarak kullanmak için GeoJSON dosyası ekleyin 😇👇",
+            type=["geojson", "kml", "zip"],
+        )
+
+        selected_roi = st.selectbox(
+            "Çalışılacak roi'yi seçin veya GeoJSON dosyası yükleyin.",
+            ["Yüklenilen GeoJSON"] + list(rois.fire_cases.keys()),
+            index=0,
+        )
+
+        pre_fire = date.today() - datetime.timedelta(days=INITIAL_DATE_WINDOW)
+        post_fire = date.today()
+
+        if selected_roi != "Yüklenilen GeoJSON":  # rois coming from fire_cases
+            st.session_state["roi"] = rois.fire_cases[selected_roi]["region"]
