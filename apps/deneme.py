6d4979baa92e9c3a2d2e6e6fae8026a84c8228e2
Efe Can Kırbıyık <81926833+EFCK@users.noreply.github.com>
2022-07-08 15:39:04 +0300

    Update deneme.py

--- apps/deneme.py
+++ apps/deneme.py
@@ -1,7 +1,7 @@
-import json
 
 
-with open(r"C:\Users\krbyk\Desktop\data.geojson","r") as f:
-    data = json.loads(f.read())
 
-print(data["features"][0]["geometry"]["coordinates"][0])
+with open(r"C:\Users\krbyk\Desktop\Dortyol_Yangin.kml","r") as f:
+    data = f.read()
+
+print(data.find("coordinates"))
