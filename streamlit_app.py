57ee0ec70cc8ddd78544dd159a207a25384b5537
Osman F Bayram <74963545+osbm@users.noreply.github.com>
2022-07-11 02:10:06 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ Streamlit App
 import streamlit as st
 from PIL import Image
 from streamlit_option_menu import option_menu
+
 from apps import fire_analysis, home, timelapse
 
 st.set_page_config(page_title="Yangın Analizi", page_icon="🔥", layout="wide")
